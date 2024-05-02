3fe410f670aa78926537ed7baae131614bdef867
Eemil <eemil.nurmi@gmail.com>
2024-05-02 17:20:23 +0300

    pelin pyörityksen tekoa ja sen vaatimia funktioita

---
+++ Frontend/functionalities.js
@@ -0,0 +1,47 @@
+import * as fetch from './fetchFuncs';
+function planeModel() {
+  while (true) {
+    let airplane_model = prompt('Enter airplane model: (1/2/3)');
+
+    if (airplane_model === '1') {
+      airplane_model = 'Boeing 737';
+      break;
+    } else if (airplane_model === '2') {
+      airplane_model = 'Airbus A320';
+      break;
+    } else if (airplane_model === '3') {
+      airplane_model = 'Saab JA37 Viggen';
+      break;
+    } else if (airplane_model === 'Matti' || airplane_model === 'Peyman') {
+      airplane_model = 'Matti or Peyman';
+      break;
+    } else {
+      alert('Invalid airplane model. Please try again.');
+    }
+  }
+
+  console.log('Selected airplane model:', airplane_model);
+  return airplane_model;
+}
+
+function plane_info() {
+  const info = 'Boeing 737:\n' +
+      'The Boeing 737 is a popular narrow-body aircraft produced by Boeing Commercial Airplanes.\n' +
+      'It is commonly used for short to medium-haul flights and is one of the best-selling commercial jetliners in history.\n' +
+      'The Boeing 737 has several variants, each with different seating capacities and range capabilities.\n' +
+      'Airbus A320:\n' +
+      'The Airbus A320 is a narrow-body airliner developed by Airbus.\n' +
+      'It is widely used by airlines around the world for short to medium-haul flights.\n' +
+      'Like the Boeing 737, the Airbus A320 has several variants, including the A318, A319, A320, and A321, each with varying seating capacities and range capabilities.\n' +
+      'Saab JA37 Viggen:\n' +
+      'The Saab JA37 Viggen is not a commercial airliner like the Boeing 737 and Airbus A320. Instead, it is a Swedish single-seat, single-engine, short-medium range combat aircraft.\n' +
+      'The Viggen was developed by Saab in the 1960s to replace the aging Saab 35 Draken as the Swedish Air Force\'s primary fighter aircraft.\n' +
+      'It features advanced avionics and was designed to perform a variety of roles, including air defense, ground attack, and reconnaissance.';
+  console.log(info)
+  alert(info)
+}
+
+function quiz(){
+  let question = func.question();
+
+}
--- Frontend/main.js
+++ Frontend/main.js
@@ -1,15 +1,45 @@
-import * as fetch from 'fetchFuncs';
-
-
+import * as fetch from './fetchFuncs';
+import * as func from './functionalities';
 
 // LEAFLET MÄPPI KOODI
 const map = L.map('map').setView([60.223831106748996, 24.758107289511884], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([60.223831106748996, 24.758107289511884]).addTo(map)
-    .bindPopup('KARAMALMI JAUUUU')
-    .openPopup();
+L.marker([60.223831106748996, 24.758107289511884]).
+    addTo(map).
+    bindPopup('KARAMALMI JAUUUU').
+    openPopup();
 // -----
+
+// pelin pyöritys
+let score = 0;
+let distance = 0;
+let used_time = 0;
+let co2_used = 0;
+let current_airport = gamesql.random_fly();
+let game_going = True;
+let airplane_model = '';
+airplane_model = ''
+let buttonClicked = false;
+
+document.getElementById('quit_button').
+    addEventListener('click', function(event) {
+      buttonClicked = true;
+    });
+
+while (!buttonClicked) {
+  // plane selection
+  const selection1 = confirm(
+      'Do you want info of our plane models before choosing?');
+  if (selection1 === true) {
+    func.plane_info();
+  } else {
+    break;
+  }
+  airplane_model = func.planeModel()
+
+}
+
