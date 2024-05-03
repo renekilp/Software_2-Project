1bbc3f3a70bd55ac06de92cd6382d705d61d44c6
onnik <onni.kivinen@gmail.com>
2024-05-03 10:52:51 +0300

    tweakattu map pinin etäisyyttä

--- Backend/static/main.js
+++ Backend/static/main.js
@@ -166,7 +166,7 @@ async function runGame() {
   let quitButtonClicked = false;
 
   // ALOITUS LENTOKENTÄN KARTTA-PIN
-  const map = L.map('map').setView([currentLatitude, currentLongitude], 13);
+  const map = L.map('map').setView([currentLatitude, currentLongitude], 12);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
