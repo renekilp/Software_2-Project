2e4e8020a442766d44d333bf5347ba4a75efddc8
onnik <onni.kivinen@gmail.com>
2024-05-04 11:29:38 +0300

    lisää shittii

--- Backend/static/main.js
+++ Backend/static/main.js
@@ -1,25 +1,25 @@
 async function airports() {
   try {
     //kokeilee hakea tietoa
-    const response = await fetch('http://127.0.0.1:3000/large_airports');
+    const response = await fetch("http://127.0.0.1:3000/large_airports");
     const data = await response.json();
     // console.log(data);
     return data; //palauttaa fetchillä haetut tiedot
   } catch (error) {
     // keskeyttää jos tapahtuu error
-    console.error('Error fetching airports:', error);
+    console.error("Error fetching airports:", error);
     throw error; //lopettaa promisen errorin takia
   }
 }
 
 async function questions() {
   try {
-    const response = await fetch('http://127.0.0.1:3000/random_question');
+    const response = await fetch("http://127.0.0.1:3000/random_question");
     const data = await response.json();
     console.log(data);
     return data;
   } catch (error) {
-    console.error('Error fetching random question:', error);
+    console.error("Error fetching random question:", error);
     throw error;
   }
 }
@@ -32,19 +32,19 @@ async function addScore(playerName, score) {
     console.log(data);
     return data;
   } catch (error) {
-    console.error('Error adding score:', error);
+    console.error("Error adding score:", error);
     throw error;
   }
 }
 
 async function randomFly() {
   try {
-    const response = await fetch('http://127.0.0.1:3000/random_flight');
+    const response = await fetch("http://127.0.0.1:3000/random_flight");
     const data = await response.json();
     // console.log(data);
     return data;
   } catch (error) {
-    console.error('Error fetching random question:', error);
+    console.error("Error fetching random question:", error);
     throw error;
   }
 }
@@ -57,19 +57,19 @@ async function travelingCo2(userAirport, airplaneModel) {
     console.log(data);
     return data;
   } catch (error) {
-    console.error('Error fetching CO2 data:', error);
+    console.error("Error fetching CO2 data:", error);
     throw error;
   }
 }
 
 async function topPlayers() {
   try {
-    const response = await fetch('http://127.0.0.1:3000/top_players');
+    const response = await fetch("http://127.0.0.1:3000/top_players");
     const data = await response.json();
     console.log(data);
     return data;
   } catch (error) {
-    console.error('Error fetching random question:', error);
+    console.error("Error fetching random question:", error);
     throw error;
   }
 }
@@ -77,43 +77,43 @@ async function topPlayers() {
 
 function planeModel() {
   while (true) {
-    let airplaneModel = prompt('Enter airplane model: (1/2/3)');
+    let airplaneModel = prompt("Enter airplane model: (1/2/3)");
 
-    if (airplaneModel === '1') {
-      airplaneModel = 'Boeing 737';
+    if (airplaneModel === "1") {
+      airplaneModel = "Boeing 737";
       break;
-    } else if (airplaneModel === '2') {
-      airplaneModel = 'Airbus A320';
+    } else if (airplaneModel === "2") {
+      airplaneModel = "Airbus A320";
       break;
-    } else if (airplaneModel === '3') {
-      airplaneModel = 'Saab JA37 Viggen';
+    } else if (airplaneModel === "3") {
+      airplaneModel = "Saab JA37 Viggen";
       break;
-    } else if (airplaneModel === 'Matti' || airplaneModel === 'Peyman') {
-      airplaneModel = 'Matti or Peyman';
+    } else if (airplaneModel === "Matti" || airplaneModel === "Peyman") {
+      airplaneModel = "Matti or Peyman";
       break;
     } else {
-      alert('Invalid airplane model. Please try again.');
+      alert("Invalid airplane model. Please try again.");
     }
   }
 
-  console.log('Selected airplane model:', airplaneModel);
+  console.log("Selected airplane model:", airplaneModel);
   return airplaneModel;
 }
 
 function planeInfo() {
   const info =
-    'Boeing 737:\n' +
-    'The Boeing 737 is a popular narrow-body aircraft produced by Boeing Commercial Airplanes.\n' +
-    'It is commonly used for short to medium-haul flights and is one of the best-selling commercial jetliners in history.\n' +
-    'The Boeing 737 has several variants, each with different seating capacities and range capabilities.\n' +
-    'Airbus A320:\n' +
-    'The Airbus A320 is a narrow-body airliner developed by Airbus.\n' +
-    'It is widely used by airlines around the world for short to medium-haul flights.\n' +
-    'Like the Boeing 737, the Airbus A320 has several variants, including the A318, A319, A320, and A321, each with varying seating capacities and range capabilities.\n' +
-    'Saab JA37 Viggen:\n' +
-    'The Saab JA37 Viggen is not a commercial airliner like the Boeing 737 and Airbus A320. Instead, it is a Swedish single-seat, single-engine, short-medium range combat aircraft.\n' +
+    "Boeing 737:\n" +
+    "The Boeing 737 is a popular narrow-body aircraft produced by Boeing Commercial Airplanes.\n" +
+    "It is commonly used for short to medium-haul flights and is one of the best-selling commercial jetliners in history.\n" +
+    "The Boeing 737 has several variants, each with different seating capacities and range capabilities.\n" +
+    "Airbus A320:\n" +
+    "The Airbus A320 is a narrow-body airliner developed by Airbus.\n" +
+    "It is widely used by airlines around the world for short to medium-haul flights.\n" +
+    "Like the Boeing 737, the Airbus A320 has several variants, including the A318, A319, A320, and A321, each with varying seating capacities and range capabilities.\n" +
+    "Saab JA37 Viggen:\n" +
+    "The Saab JA37 Viggen is not a commercial airliner like the Boeing 737 and Airbus A320. Instead, it is a Swedish single-seat, single-engine, short-medium range combat aircraft.\n" +
     "The Viggen was developed by Saab in the 1960s to replace the aging Saab 35 Draken as the Swedish Air Force's primary fighter aircraft.\n" +
-    'It features advanced avionics and was designed to perform a variety of roles, including air defense, ground attack, and reconnaissance.';
+    "It features advanced avionics and was designed to perform a variety of roles, including air defense, ground attack, and reconnaissance.";
   console.log(info);
   alert(info);
 }
@@ -142,8 +142,8 @@ function quiz() {
     [answers[i], answers[j]] = [answers[j], answers[i]];
   }
   // console logeja debugaamista varten
-  console.log('Question:', question);
-  console.log('Shuffled Answers:');
+  console.log("Question:", question);
+  console.log("Shuffled Answers:");
   answers.forEach((answer, index) => {
     console.log(`${index + 1}. ${answer}`);
   });
@@ -166,7 +166,7 @@ async function runGame() {
   let usedTime = 0;
   let co2Used = 0;
   let airportData = await randomFly();
-  let currentAirport = airportData['airport name'];
+  let currentAirport = airportData["airport name"];
   let currentLatitude = airportData.latitude;
   let currentLongitude = airportData.longitude;
 
@@ -178,8 +178,8 @@ async function runGame() {
   let quitButtonClicked = false;
 
   // ALOITUS LENTOKENTÄN KARTTA-PIN näkymä
-  const map = L.map('map').setView([currentLatitude, currentLongitude], 12);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  const map = L.map("map").setView([currentLatitude, currentLongitude], 12);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
@@ -194,7 +194,7 @@ async function runGame() {
   const largeAirports = await airports();
 
   for (let airport of largeAirports) {
-    let largeAirportName = airport['airport name'];
+    let largeAirportName = airport["airport name"];
     let largeAirportLatitude = airport["latitude"];
     let largeAirportLongitude = airport["longitude"];
     L.marker([largeAirportLatitude, largeAirportLongitude])
@@ -206,14 +206,14 @@ async function runGame() {
   // __________________________________________
 
   document
-    .getElementById('quit_button')
-    .addEventListener('click', function (event) {
+    .getElementById("quit_button")
+    .addEventListener("click", function (event) {
       quitButtonClicked = true;
     });
 
   while (!quitButtonClicked) {
     const selection1 = confirm(
-      'Do you want info of our plane models before choosing?'
+      "Do you want info of our plane models before choosing?"
     );
     if (selection1 === true) {
       planeInfo();
@@ -223,37 +223,37 @@ async function runGame() {
     const airplaneModel = planeModel();
 
     document
-      .getElementById('start_game_button')
-      .addEventListener('click', function (event) {
+      .getElementById("start_game_button")
+      .addEventListener("click", function (event) {
         airportData = randomFly();
-        currentAirport = airportData['airport name'];
-        currentAirport = airportData['airport name'];
-        currentLatitude = airportData['latitude'];
-        currentLongitude = airportData['longitude'];
+        currentAirport = airportData["airport name"];
+        currentAirport = airportData["airport name"];
+        currentLatitude = airportData["latitude"];
+        currentLongitude = airportData["longitude"];
         let travelData = travelingCo2(currentAirport, airplaneModel);
         distance = distance + travelData.distance;
         co2Used = co2Used + travelData.co2;
         usedTime = distance + travelData.flight_time;
         let questioner = quiz();
         let userAnswer = prompt(
-          questioner.question + '\n' + questioner.answers
+          questioner.question + "\n" + questioner.answers
         );
         if (userAnswer === questioner.correct_answer) {
           score += 1;
-          alert('Correct!');
+          alert("Correct!");
         } else {
-          alert('Incorrect!');
+          alert("Incorrect!");
         }
       });
     document
-      .getElementById('save_score')
-      .addEventListener('click', function (event) {
-        let name = prompt('What is your username?');
+      .getElementById("save_score")
+      .addEventListener("click", function (event) {
+        let name = prompt("What is your username?");
         addScore(name, score);
       });
     document
-      .getElementById('top_players')
-      .addEventListener('click', function (event) {
+      .getElementById("top_players")
+      .addEventListener("click", function (event) {
         let tPlayers = topPlayers();
         alert(tPlayers);
       });
@@ -262,34 +262,41 @@ async function runGame() {
 
 // ----- Tutorial popup soosit --------
 
-let popup =document.getElementById("helppop");
+let popup = document.getElementById("helppop");
 
-function openPopup(){
+function openPopup() {
   popup.classList.add("open-helppop");
 }
 
-function closePopup(){
+function closePopup() {
   popup.classList.remove("open-helppop");
 }
-
 
 // PELIN START NAPPI
 const gameStartWindow = document.getElementById("startButton");
 const gameStartButton = document.getElementById("startGame");
 
-window.onload = function() { //start nappulan ikkuna
+window.onload = function () {
+  //start nappulan ikkuna
   gameStartWindow.style.display = "block";
-}
+};
 
-gameStartButton.onclick = function() { // starttinappulan jälkeen display none
-  gameStartWindow.style.display = "none"; 
-  
-  // sit kysytään nimi, tsekataan että se ei ole tyhjä, ja että jotain on syötetty
-  let name = prompt('What is your username?');
-  while (name == null || name.trim() === "") {
-    name = prompt('Please enter a valid username:');
+gameStartButton.onclick = function () {
+  // starttinappulan jälkeen display none
+  gameStartWindow.style.display = "none";
+
+  function capitalize(str) { // funktio että voi saada ekan kirjaimen isoksi
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
   }
-  console.log("Welcome, " + name + "!");
 
-  runGame(); //AJETAAN PELI
-}
+  // sit kysytään nimi, tsekataan että se ei ole tyhjä, ja että jotain on syötetty
+  let name = prompt("What is your username?");
+  while (name == null || name.trim() === "") {
+    name = prompt("Please enter a valid username:");
+  }
+  name = capitalize(name);
+  console.log("Welcome, " + name + "!");
+  document.querySelector("#playerName").textContent = name;
+
+  runGame(); //Startin ja nimen jälkeen alotetaan peli
+};
